30c20e8a1fc664ada571f30c2200cc5bdee45ea1
aji <ajizkai1301@gmail.com>
2022-04-28 10:32:25 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,76 @@
+import os
+import sys
+from slack import WebClient
+from turner import Turner
+
+
+
+# class initializer for command line arguments
+class Main():
+    def __init__(self):
+        pass
+    
+    def readCommand(self):
+        ans = sys.argv[1]
+        return ans
+    
+    def tagsDecider(self):
+        read = self.readCommand()
+        if read == 'up':
+            tags = 'StartUp'
+        elif read == 'down':
+            tags = 'ShutDown'
+        else:
+            print('Wrong command')
+        return tags
+
+# class begining of the action
+class Action(Main):
+    def __init__(self):
+        Main.__init__(self)
+        self.tags = self.tagsDecider()
+    
+    def action(self):
+        t = Turner(self.tags)
+        msg = t.action()
+        return msg
+
+# class for slack message
+class Slack_client(Turner, Main):
+    def __init__(self, msg):
+        Turner.__init__(self, self.tagsDecider())
+
+        self.msg = msg
+        self.field = Turner.formatMsg(self)
+        pass
+
+    def notify(self):
+        sclient = WebClient(token=os.environ['SLACK_TOKEN'])
+
+        blocks = [
+                    {
+                        "type": "context",
+                        "elements": [
+                            {
+                                "type": "mrkdwn",
+                                "text": "List of servers " + self.msg + " by Automation AWS: "
+                            }
+                        ]
+                    },
+                    {
+                        "type": "divider"
+                    },
+                    {
+                        "type": "section",
+                        "fields": self.field
+                    }
+                ]
+
+        sclient.chat_postMessage(channel='#'+ os.environ['CHANNEL'], blocks=blocks)
+
+if __name__ == '__main__':
+    a = Action()
+    msg = a.action()
+
+    s = Slack_client(msg)
+    s.notify()
---
+++ turner.py
@@ -0,0 +1,87 @@
+import boto3
+
+# construction of the client
+client = boto3.client('ec2', region_name='ap-southeast-1')
+
+# class for get the instance id and name
+class Wrapper(object):
+    def __init__(self, tags):
+        self.tags = tags
+        pass
+
+    def filterInstanceIds(self):
+        response = client.describe_instances(
+        Filters=[
+            {
+            'Name': 'tag:' + self.tags,
+            'Values': [
+                'True',
+            ]
+            }
+        ]
+        )
+        return response
+
+    instance_ids = []
+    list_name = []
+    def getInstancesIds(self):
+        response = self.filterInstanceIds()
+        for i in response['Reservations']:
+            for j in i['Instances']:
+                self.instance_ids.append(j['InstanceId'])
+                for x in j['Tags']:
+                    if x['Key'] == 'Name':
+                        self.list_name.append(x['Value'])
+        return self.instance_ids, self.list_name
+
+# class for turn on and off the instance and formatting slack message
+class Turner(Wrapper):
+    def __init__(self, tags):
+        Wrapper.__init__(self, tags)
+        self.getId = self.getInstancesIds()
+        self.instance_ids = self.getId[0]
+        self.list_name = self.getId[1]
+    
+    def action(self):
+        if self.tags == 'StartUp':
+            msg = 'turned on'
+            self.turnon()
+        elif self.tags == 'ShutDown':
+            msg = 'turned off'
+            self.turnoff()
+        return msg
+
+    def formatMsg(self):
+        if len(self.list_name) == 0:
+            field = [{
+                "type": "plain_text",
+                "text": "No instance",
+                "emoji": True
+            }]
+        else:
+            field = []
+            for y in self.list_name:
+                z = {
+                    "type": "plain_text",
+                    "text": y,
+                    "emoji": True
+            }
+            field.append(z)
+        return field
+
+    def turnon(self):
+        for a in self.instance_ids:
+                client.start_instances(
+                    InstanceIds=[
+                        a
+                    ]
+                )
+
+    def turnoff(self):
+        for a in self.instance_ids:
+                client.stop_instances(
+                    InstanceIds=[
+                        a
+                    ]
+                )
+
